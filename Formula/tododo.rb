7468877b4a4492cbd363a35cfb6d5fa6d249dc53
Brandon Martinez <bmartheswe@gmail.com>
2025-08-19 19:41:37 -0500

    Linting

--- Formula/tododo.rb
+++ Formula/tododo.rb
@@ -1,5 +1,5 @@
 class Tododo < Formula
-  desc "A todo manager that should be extinct"
+  desc "Some todo manager that should be extinct"
   homepage "https://github.com/bmarse/tododo"
   url "https://github.com/bmarse/tododo/archive/refs/tags/v0.5.0-stable.tar.gz"
   sha256 "50ff5ef2c5ba1d463aba0def28f9439b59ca87bc354bc5150df67ffe12ac2576"
