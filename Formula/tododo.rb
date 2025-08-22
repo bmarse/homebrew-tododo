979aaefab6b30b653d649b623b5ef9983edf144b
Brandon Martinez <bmartheswe@gmail.com>
2025-08-21 20:11:25 -0500

    Upgrading to v0.5.1

--- Formula/tododo.rb
+++ Formula/tododo.rb
@@ -1,8 +1,8 @@
 class Tododo < Formula
   desc "Some todo manager that should be extinct"
   homepage "https://github.com/bmarse/tododo"
-  url "https://github.com/bmarse/tododo/archive/refs/tags/v0.5.0-stable.tar.gz"
-  sha256 "50ff5ef2c5ba1d463aba0def28f9439b59ca87bc354bc5150df67ffe12ac2576"
+  url "https://github.com/bmarse/tododo/archive/refs/tags/v0.5.1-stable.tar.gz"
+  sha256 "9457ea2901a7cf97f319fa886553cc8c5317b607e15d9eae254e3829e729e94f"
   license "MIT"
   depends_on "go" => :build
   def install
